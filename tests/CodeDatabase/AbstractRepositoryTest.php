1352d5c0dae0a73852577608a3c148486f2ea953
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-05 11:12:56 -0300

    Mock de repository-findBy

--- tests/CodeDatabase/AbstractRepositoryTest.php
+++ tests/CodeDatabase/AbstractRepositoryTest.php
@@ -32,8 +32,10 @@ class AbstractRepositoryTest extends AbstractTestCase
         $mockRepository
                 ->shouldReceive('all')
                 ->andReturn([$mockStd, $mockStd, $mockStd]);
-        $this->assertCount(3, $mockRepository->all());
-        $this->assertInstanceOf(\stdClass::class, $mockRepository->all()[0]);
+        
+        $result = $mockRepository->all();
+        $this->assertCount(3, $result);
+        $this->assertInstanceOf(\stdClass::class, $result[0]);
     }
     
     public function test_should_return_all_with_arguments()
@@ -47,8 +49,10 @@ class AbstractRepositoryTest extends AbstractTestCase
                 ->shouldReceive('all')
                 ->with(['id', 'name'])
                 ->andReturn([$mockStd, $mockStd, $mockStd]);
-        $this->assertCount(3, $mockRepository->all(['id', 'name']));
-        $this->assertInstanceOf(\stdClass::class, $mockRepository->all(['id', 'name'])[0]);
+        
+        $result = $mockRepository->all(['id', 'name']);
+        $this->assertCount(3, $result);
+        $this->assertInstanceOf(\stdClass::class, $result[0]);
     }
     
     public function test_should_return_create()
@@ -181,6 +185,38 @@ class AbstractRepositoryTest extends AbstractTestCase
                 ->andThrow($throw);
         
         $mockRepository->find(0);
+    }
+    
+    public function test_should_return_findby_with_columns_success()
+    {
+        $mockRepository = m::mock(AbstractRepository::class);
+        $mockStd = m::mock(\stdClass::class);
+        $mockStd->id = 1;
+        $mockStd->name = 'name';
+        
+        $mockRepository
+                ->shouldReceive('findBy')
+                ->with('name', 'my-data', ['id', 'name'])
+                ->andReturn([$mockStd, $mockStd, $mockStd]);
+        
+        $result = $mockRepository->findBy('name', 'my-data', ['id', 'name']);
+                
+        $this->assertCount(3, $result);
+        $this->assertInstanceOf(\stdClass::class, $result[0]);
+    }
+    
+    public function test_should_return_findby_empty_success()
+    {
+        $mockRepository = m::mock(AbstractRepository::class);
+        
+        $mockRepository
+                ->shouldReceive('findBy')
+                ->with('name', '', ['id', 'name'])
+                ->andReturn([]);
+        
+        $result = $mockRepository->findBy('name', '', ['id', 'name']);
+                
+        $this->assertCount(0, $result);
     }
 
 }
