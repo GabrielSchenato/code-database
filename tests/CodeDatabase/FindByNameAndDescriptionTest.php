57c14f5d95cf0daff72539098903c5f46aeaa2b3
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-09 11:56:22 -0300

    Testando Stub Criteria

--- tests/CodeDatabase/FindByNameAndDescriptionTest.php
+++ tests/CodeDatabase/FindByNameAndDescriptionTest.php
@@ -7,6 +7,7 @@ use CodePress\CodeDatabase\Repository\CategoryRepository;
 use CodePress\CodeDatabase\Models\Category;
 use CodePress\CodeDatabase\Criteria\FindByNameAndDescription;
 use CodePress\CodeDatabase\Contracts\CriteriaInterface;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Description of CategoryTest
@@ -20,7 +21,9 @@ class FindByNameAndDescriptionTest extends AbstractTestCase
      * @var CategoryRepository 
      */
     private $repository;
-    
+    /**
+     * @var FindByNameAndDescription 
+     */
     private $criteria;
 
     public function setUp()
@@ -28,13 +31,28 @@ class FindByNameAndDescriptionTest extends AbstractTestCase
         parent::setUp();
         $this->migrate();
         $this->repository = new CategoryRepository();
-        $this->criteria = new FindByNameAndDescription();
+        $this->criteria = new FindByNameAndDescription('Category 1', 'Description 1');
         $this->createCategory();
     }
 
     public function test_if_instanceof_criteriainterface()
     {
         $this->assertInstanceOf(CriteriaInterface::class, $this->criteria);
+    }
+    
+    public function test_if_apply_returns_querybuilder()
+    {
+        $class = $this->repository->model();
+        $result = $this->criteria->apply(new $class, $this->repository);
+        $this->assertInstanceOf(Builder::class, $result);
+    }
+    
+    public function test_if_apply_returns_data()
+    {
+        $class = $this->repository->model();
+        $result = $this->criteria->apply(new $class, $this->repository)->get()->first();
+        $this->assertEquals('Category 1', $result->name);
+        $this->assertEquals('Description 1', $result->description);
     }
 
     private function createCategory()
--- tests/stub/Criteria/FindByNameAndDescription.php
+++ tests/stub/Criteria/FindByNameAndDescription.php
@@ -12,10 +12,18 @@ use CodePress\CodeDatabase\Contracts\RepositoryInterface;
  */
 class FindByNameAndDescription implements CriteriaInterface
 {
+    private $name;
+    private $description;
+    
+    public function __construct(string $name, string $description)
+    {
+        $this->name = $name;
+        $this->description = $description;
+    }
 
     public function apply($model, RepositoryInterface $repository)
     {
-        
+        return $model->where('name', $this->name)->where('description', $this->description);
     }
 
 }
