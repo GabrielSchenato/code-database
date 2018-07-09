c7e19392a1dd9d771873413fe774ccddb25347e4
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-09 14:23:23 -0300

    Criando CriteriaCollectionInterface

---
+++ src/CodeDatabase/Contracts/CriteriaCollectionInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CodePress\CodeDatabase\Contracts;
+
+/**
+ *
+ * @author gabriel
+ */
+interface CriteriaCollectionInterface
+{
+
+    public function addCriteria(CriteriaInterface $criteria);
+
+    public function getCriteriaCollection();
+
+    public function getByCriteria(CriteriaInterface $criteriaInterface);
+
+    public function applyCriteria();
+}
